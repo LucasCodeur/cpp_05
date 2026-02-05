662d9092b0d181967fba72e0bd53399454c47ce1
LucasCodeur <l.dadamopro@gmail.com>
2026-02-05 17:43:40 +0100

    FEAT: finish three different class with test

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 16:07:41 by lud-adam          #+#    #+#             */
-/*   Updated: 2026/02/05 17:31:41 by lud-adam         ###   ########.fr       */
+/*   Updated: 2026/02/05 17:42:58 by lud-adam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ static	void testAsciiTreeFormShruberry(void);
 static	void testRand(void);
 static	void testRandomMessage(void);
 static	void testPardonPresidential(void);
+static	void testRobotomyConstructors(void);
+static	void testPresidentialConstructors(void);
 
 int	main(void)
 {
@@ -37,17 +39,23 @@ int	main(void)
 	// testRand();
 	// testRandomMessage();
 	// testPardonPresidential();
+	// testRobotomyConstructors();
+	testPresidentialConstructors();
 	return (0);
 }
 
 static	void testConstructorShrubbery(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testConstructorShrubbery" << std::endl;
 	ShrubberyCreationForm	test;
 	std::cout << test << std::endl;
 }
 
 static	void testCopyConstructorShrubbery(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testCopyConstructorShrubbery" << std::endl;
 	ShrubberyCreationForm	test;
 
 	std::cout << test << std::endl;
@@ -59,6 +67,8 @@ static	void testCopyConstructorShrubbery(void)
 
 static	void testParameterizedConstructorShrubbery(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testParameterizedConstructorShrubbery" << std::endl;
 	ShrubberyCreationForm	test("Albert");
 
 	std::cout << test << std::endl;
@@ -66,6 +76,8 @@ static	void testParameterizedConstructorShrubbery(void)
 
 static	void testOperatorEqualShrubbery(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testOperatorEqualShrubbery" << std::endl;
 	ShrubberyCreationForm	test("Albert");
 	ShrubberyCreationForm	test1("Albert");
 
@@ -77,12 +89,16 @@ static	void testOperatorEqualShrubbery(void)
 
 static void testAsciiTreeFormShruberry(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testAsciiTreeFormShruberry" << std::endl;
 	ShrubberyCreationForm	test("Albert");
 	test.createTrees("Mojo_tree");
 }
 
 static void	testRand(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testRand" << std::endl;
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << std::rand() % 2 << std::endl;
@@ -91,20 +107,47 @@ static void	testRand(void)
 
 static void testRandomMessage(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testRandomMessage" << std::endl;
 	RobotomyRequestForm	test("Albert");
 	test.Robotomy("Bob");
 }
 
 static void testPardonPresidential(void)
 {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "testPardonPresidential" << std::endl;
 	PresidentialPardonForm test;
 
 	test.pardoned("Bob");
 }
 
-static void	testConstructors(void)
+static void	testRobotomyConstructors(void)
 {
-	ShrubberyCreationForm test1;
-	PresidentialPardonForm test2;
+	std::cout << "testRobotomyConstructors" << std::endl;
 	RobotomyRequestForm test3;
+	std::cout << test3 << std::endl;
+	RobotomyRequestForm test4("Mario");
+	std::cout << test4 << std::endl;
+	RobotomyRequestForm test5(test4);
+	std::cout << test5 << std::endl;
+	RobotomyRequestForm test10;
+	std::cout << test10 << std::endl;
+	test10 = test4;
+	std::cout << test10 << std::endl;
+}
+
+static void	testPresidentialConstructors(void)
+{
+	std::cout << "testPresidentialConstructors" << std::endl;
+	PresidentialPardonForm test3;
+	std::cout << test3 << std::endl;
+	PresidentialPardonForm test4("Mario");
+	std::cout << test4 << std::endl;
+	PresidentialPardonForm test5(test4);
+	std::cout << test5 << std::endl;
+	PresidentialPardonForm test10;
+	std::cout << test10 << std::endl;
+	test10 = test4;
+	std::cout << test10 << std::endl;
 }
